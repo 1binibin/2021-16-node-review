f39e83e22b057149942a87e64ae92a427f39ea84
1binibin <namwonbin@gmail.com>
2021-09-03 11:21:56 +0900

    ...로직

---
+++ routes/member/login-router.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+
+// http://127.0.0.1:3000/member
+
+router.get('/', (req, res, next) => {
+
+})
+
+router.use('/login', (req, res, next) => {
+
+})
+
+
+module.exports = router
